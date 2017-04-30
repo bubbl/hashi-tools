1fb256f1d7de7aea37adee99fe85c6e04b3b27b7
Bart Bania <contact@bartbania.com>
2017-04-30 20:41:53 +0100

    init

--- consul/config/entrypoint.sh
+++ consul/config/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/dumb-init /bin/sh
 set -e
 
-gosu consul '/bin/consul agent -server -config-dir=/consul/config'
+gosu consul /bin/consul agent -server -config-dir=/consul/config -data-dir=/consul/data
 
